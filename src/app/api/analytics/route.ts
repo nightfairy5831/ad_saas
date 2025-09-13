3a8d4ae37edf0b5fa2839d879ec96cface898903
nightmare <vadympykivskyi@gmail.com>
2025-09-13 16:27:48 +0000

    fix:update anlaytics endpoint to ensure dynamic routing

--- src/app/api/analytics/route.ts
+++ src/app/api/analytics/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { getUserFromRequest } from '@/lib/auth'
 import { prisma } from '@/lib/prisma/db'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
   try {
     const userResult = getUserFromRequest(request)
